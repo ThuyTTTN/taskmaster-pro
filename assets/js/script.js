9c2b552779f9343aac62e5fa60a7742d7775ecf1
Thuy Nguyen <ttruong06@gmail.com>
2022-03-21 12:32:46 -0400

    drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,81 @@ var loadTasks = function() {
   });
 };
 
+// sortable() turned every element with class 'list-group' into sort list
+$(".card .list-group").sortable({
+  //connectWith links the sortable lists with other lists with same class
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  // tells jQuery to create a copy of the dragged element and move the copy instead of the original
+  helper: "clone",    
+  //event listener; triggers once for all connected lists as soon as dragging starts and stop
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  //event listener; triggers once for all connected lists as soon as dragging starts and stop
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  //event listener; triggers when a dragged item enters or leaves a connected list
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  //event listener; triggers when a dragged item enters or leaves a connected list
+  out: function(event) {
+    console.log("out", event.target);
+  }, 
+  //triggers when the contents of a list have changed (reordered, removed, added)
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    // children() method returns an array of the list element's children <li> elements
+    // each() method will run a callback function for every item/element in the array; another form of looping
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      //add task data to temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+
+      //trim down list's ID to match object property
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      //update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+    });
+    console.log(tempArr);
+  }   
+});
+
+$("#trash").droppable({
+  accept: ".car .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    // ui is a variable object that contains 'draggable'; remove() remove element from the DOM entirely
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function (event, ui) {
+    console.log("out");
+  }
+})
+
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
